cb143bf41c9f91dcf21c3951e669f9c9619bd6ac
Aleksander <kewgine@gmail.com>
2022-09-28 20:53:33 +0300

    add block-scheme and first part of task

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] array = new string[]{"Python","C#","JavaScript","Solidity","PHP"};
+
+Console.WriteLine("Какая должна быть максимальная длина элемента в новом массиве?");
+int necessaryLength = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+for(int i = 0; i < array.Length;i++)
+{
+    if(array[i].Length <= necessaryLength) count++;
+}
+
